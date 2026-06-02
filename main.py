8169bf0cdd6307fcdc26e28824541a32ca009f1a
Manikanta <mytab123yy@gmail.com>
2026-06-02 10:21:01 +0530

    intial commit

--- main.py
+++ main.py
@@ -4,8 +4,23 @@ from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_ollama import ChatOllama
 from langchain_tavily import TavilySearch
+from pydantic import BaseModel, Field 
+from typing import List
 import os 
 load_dotenv()
+class Source(BaseModel):
+    """
+    Schema for a source used by an agent 
+    """
+    url : str = Field(description="The Url of the source ")
+
+
+class AgentResponse(BaseModel):
+    """
+    Schema for agent response with a answer and sources 
+    """
+    answer : str = Field(description="the agent's answer to the query ")
+    sources : List[Source] = Field(default_factory=list,description="the list of sources to generate the answer")
 
 # from tavily import TavilyClient
 # tavily = TavilyClient()
@@ -29,13 +44,15 @@ llm = ChatOllama(
     temperature=0
 )
 tools = [TavilySearch()]
-agent = create_agent(model = llm, tools = tools)
+agent = create_agent(model = llm, tools = tools, response_format=AgentResponse)
 def main():
    pass
    result = agent.invoke({
     "messages": [
         HumanMessage(content="What is the weather in Tokyo today")
     ]
+
 })
+   print(result)
 if __name__ == "__main__":
     main()
